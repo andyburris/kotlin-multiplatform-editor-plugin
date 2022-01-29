d285b3efe3190292b1b8f60ba587b1f2325257d7
GitHub Action <action@github.com>
2022-01-29 21:23:19 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/andyburris/kotlinmultiplatformeditorplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.andyburris.kotlinmultiplatformeditorplugin.services
+
+import com.github.andyburris.kotlinmultiplatformeditorplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/andyburris/kotlinmultiplatformeditorplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/andyburris/kotlinmultiplatformeditorplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.andyburris.kotlinmultiplatformeditorplugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.andyburris.kotlinmultiplatformeditorplugin.MyBundle
 
 class MyProjectService(project: Project) {
 
